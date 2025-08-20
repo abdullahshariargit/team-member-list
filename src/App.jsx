1ae6c14a3ccc4d37ab08178e58a3f57ca69640b6
abdullahshariar <abdullahgit88@gmail.com>
2025-08-20 11:14:43 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import MemberProfile from './components/MemberProfile';
+
+const members=[
+  {
+    id: 1,
+    name: 'MD.JAKARIA',
+    role: 'Lecturer Arabic',
+    image: './src/assets/profile/Lecturer-arabic.jpg', // Local image path
+  },
+  {
+    id: 2,
+    name: 'UMMA JANNATUL FERDUSE',
+    role: 'Lecturer ICT',
+    image: './src/assets/profile/lecturer-ICT.jpg', 
+  },
+  {
+    id: 3,
+    name: 'MD. HASHIBUL ISLAM',
+    role: 'Assisatant Teacher Bengali',
+    image: './src/assets/profile/Assistant-Teacher-Bengali.jpg', 
+  },
+  {
+    id: 4,
+    name: 'NAYEEMA ISLAM',
+    role: 'Assisatant Teacher English',
+    image: './src/assets/profile/Assistant-Teacher-English.jpg',
+  },
+  {
+    id: 5,
+    name: 'HAFIZA SIKDER',
+    role: 'Assistant Teacher, Physical Science',
+    image: './src/assets/profile/Assistant-Teacher-Physcail-Science.jpg',
+  },
+  {
+    id: 6,
+    name: 'RABIUL ISLAM',
+    role: 'Assisatant Teacher Social Science',
+    image: './src/assets/profile/Assistant-teacher-Social-Science.jpg',
+  },
+  {
+    id: 7,
+    name: 'MD.ABDUL AWAL',
+    role: 'Ebtedayee Teacher  Bengali & English',
+    image: './src/assets/profile/Ebtedaye-Teacher-General.jpg',
+  },
+  
+];
+
+const App = () => {
+  return (
+    <div className='min-h-screen p-6 max-w-screen-xl  mx-auto'>
+      <h1 className='text-3xl font-bold text-center text-blue-600 mb-6'>Pirujali Amania Fazil(B.A) Madrasah Upcoming Teachers  </h1>
+      <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        {members.map(member=>(
+          <MemberProfile 
+          key={member.id}
+           name={member.name} 
+           role={member.role} 
+           image={member.image}
+           />
+           ))
+           }
+      </div>
+    </div>
+  )
+}
+
+export default App
