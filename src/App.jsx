c260afcf59ce5e30aeba1971605d3d3851a84f63
abdullahshariar <abdullahgit88@gmail.com>
2025-08-20 12:54:34 +0600

    final

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const members=[
     id: 1,
     name: 'MD.JAKARIA',
     role: 'Lecturer Arabic',
-    image: '/lecturer-arabic.jpg', // Local image path
+    image: '/arabic.jpg', // Local image path
   },
   {
     id: 2,
